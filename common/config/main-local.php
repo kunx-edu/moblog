4290bce66c0f79e885754654f4ee5bec2c049a41
mojifan <mengzhuofan@qq.com>
2015-01-24 22:53:13 +0800

    update config

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -35,19 +35,6 @@ $config= [
     ],
 ];
 
-if (!YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class'=>'yii\debug\Module',
-        'allowedIPs'=>['192.168.*'],
-    ];
 
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class'=>'yii\gii\Module',
-        'allowedIPs'=>['192.168.*'],
-    ];
-}
 
 return $config;
